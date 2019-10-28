7882f1e1d67c13af7fdeae5a90c1dfceb7929df2
Student <example@example.com>
2019-10-28 11:17:07 -0700

    create loop in javascript

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,33 @@
 $(document).ready(function() {
   $("#formOne").submit(function(event) {
-    var person1Input = $("input#person1").val();
-    var person2Input = $("input#person2").val();
-    var animalInput = $("input#animal").val();
-    var exclamationInput = $("input#exclamation").val();
-    var verbInput = $("input#verb").val();
-    var nounInput = $("input#noun").val();
+    var answers = ["person1", "person2", "animal", "exclamation", "verb", "noun"];
+    answers.forEach(function(answer) {
+      var userInput = $("#" + answer).val();
+      $("." + answer).text(userInput);
 
-    $("#person1").val("");
-    $("#person2").val("");
-    $("#animal").val("");
-    $("#exclamation").val("");
-    $("#verb").val("");
-    $("#noun").val("");
+    });
 
-
-    $(".person1").text(person1Input);
-    $(".person2").text(person2Input);
-    $(".animal").text(animalInput);
-    $(".exclamation").text(exclamationInput);
-    $(".verb").text(verbInput);
-    $(".noun").text(nounInput);
+    // var person1Input = $("input#person1").val();
+    // var person2Input = $("input#person2").val();
+    // var animalInput = $("input#animal").val();
+    // var exclamationInput = $("input#exclamation").val();
+    // var verbInput = $("input#verb").val();
+    // var nounInput = $("input#noun").val();
+    //
+    // $("#person1").val("");
+    // $("#person2").val("");
+    // $("#animal").val("");
+    // $("#exclamation").val("");
+    // $("#verb").val("");
+    // $("#noun").val("");
+    //
+    //
+    // $(".person1").text(person1Input);
+    // $(".person2").text(person2Input);
+    // $(".animal").text(animalInput);
+    // $(".exclamation").text(exclamationInput);
+    // $(".verb").text(verbInput);
+    // $(".noun").text(nounInput);
 
     $("#story").show();
 
